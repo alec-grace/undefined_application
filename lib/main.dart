c654d114dfda050d1e3adc27540f9f26b0c9e428
Alec <agrace072@protonmail.com>
2021-11-14 13:52:58 -0500

    Added curved appbar w/ bezier curve & CustomShape, made scrollable list for home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,206 +1,261 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'CustomShape.dart';
 
 void main() => runApp(MaterialApp(
+  home: OpenPage(),
+));
 
-  //----------------------------------------------------------------------------
-  // BEGIN EXPERIMENTAL SCROLL LIST FOR HOME PAGE
-  //----------------------------------------------------------------------------
+class OpenPage extends StatefulWidget {
+  @override
+  State<OpenPage> createState() => _OpenPageState();
+}
 
-  // home: SingleChildScrollView(
-  //   physics: ScrollPhysics(),
-  //   child: Column(
-  //     children: <Widget> [
-  //       Text(
-  //           'Welcome',
-  //           style: TextStyle(
-  //             fontSize: 40.0,
-  //             fontFamily: 'Urbanist',
-  //             color: Colors.blue[900],
-  //           ),
-  //       ),
-  //       ListView(
-  //         physics: NeverScrollableScrollPhysics(),
-  //         shrinkWrap: true,
-  //         children: [
-  //           Center(
-  //             child: Container(
-  //               padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-  //               child: Text(
-  //                 'Some Text',
-  //                 style: TextStyle(
-  //                   fontSize: 30.0,
-  //                   fontWeight: FontWeight.bold,
-  //                   color: Colors.blue[900],
-  //                   fontFamily: 'Urbanist',
-  //                 ),
-  //               ),
-  //             ),
-  //           ),
-  //         ],
-  //       )
-  //     ]
-  //   )
-  // )
+class _OpenPageState extends State<OpenPage> {
+  /// Data to be updated in stateful widget
+  int tracker = 0;
 
-  //----------------------------------------------------------------------------
-  // END EXPERIMENTAL SCROLL LIST FOR HOME PAGE
-  //----------------------------------------------------------------------------
-
-
-  //----------------------------------------------------------------------------
-  // BEGIN YOUTUBE TUTORIAL EXAMPLES
-  // ----------------------------------------------------------------------------
-
-    home: Scaffold(
+  @override
+  Widget build(BuildContext context) {
+    /**
+     * Widget tree to return
+     **/
+    return Scaffold(
 
     /***************************************************************************
      *  BEGIN APP BAR
      **************************************************************************/
-    // appBar: PreferredSize(
-    //   preferredSize: Size.fromHeight(50.0),
-    //   child: AppBar(
-    //     title: Text(
-    //       'SAC',
-    //       style: TextStyle(
-    //         fontFamily: 'Urbanist',
-    //       ),
-    //     ),
-    //     centerTitle: true,
-    //     backgroundColor: Colors.blue[900],
-    //   ),
-    // ),
+    appBar:
+      AppBar(
+        toolbarHeight: 150,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        flexibleSpace: ClipPath(
+          clipper: CustomShape(),
+          child: Container(
+            height: 250,
+            width: MediaQuery.of(context).size.width,
+            color: CupertinoColors.white,
+            child: Center(child: Text( //Change this to SAC logo - talk to graphic
+              //design people about making/altering one of the circle logos
+              'SAC',
+              style: TextStyle(
+                fontSize: 45.0,
+                fontFamily: 'Urbanist',
+                color: Colors.blue[900],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            ),
+          ),
+        ),
+      ),
     /***************************************************************************
      * END APP BAR
      ************************************************************************/
-    //--------------------------------------------------------------------------
-    // ADDING IMAGE EXAMPLE
-    //--------------------------------------------------------------------------
-    // body: Center(
-    //   child: Image.asset('assets/sac_shield2.jpg'),
-    // ),
-    //--------------------------------------------------------------------------
-    // END ADDING IMAGE EXAMPLE
-    //--------------------------------------------------------------------------
     backgroundColor: Colors.blue[900],
-      body:
-        Row (
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+    body:
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0),
+        child: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget> [
-            SizedBox(width: 10.0),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget> [
-                SizedBox(height: 40,),
-                ElevatedButton.icon(
-                    onPressed: (){},
-                    icon: Container(
-                        alignment: Alignment.topLeft,
-                        child: Icon(
-                          CupertinoIcons.calendar,
-                          size: 50,
-                          color: Colors.blue[900],
-                        )
+            SizedBox(height: 50.0),
+            ElevatedButton.icon(
+              onPressed: () {
+                setState(() {
+                  tracker += 1;
+                });
+              },
+              icon: Container(
+                alignment: Alignment.topLeft,
+                child: Icon(
+                  CupertinoIcons.calendar,
+                  size: 50,
+                  color: Colors.blue[900],
+                )
+              ),
+                label:
+                  Text(
+                    'Hello x$tracker',
+                    style: TextStyle(
+                      fontFamily: 'Urbana',
+                      color: Colors.blue[900],
+                      fontSize: 20
                     ),
-                    label:
-                      Text(
-                        'Hello',
-                        style: TextStyle(
-                          fontFamily: 'Urbana',
-                          color: Colors.blue[900],
-                          fontSize: 20
-                        ),
-                      ),
-                    style:
-                      ElevatedButton.styleFrom(
-                        minimumSize: const Size(375.0, 120.0),
-                        primary: Colors.white60,
-                      ),
+                  ),
+                style:
+                  ElevatedButton.styleFrom(
+                    minimumSize: const Size(375.0, 100.0),
+                    primary: Colors.white,
+                  ),
 
-                ),
-                SizedBox(height: 15.0),
-                ElevatedButton.icon(
-                    icon: Container(
-                        alignment: Alignment.center,
-                        child: Icon(
-                          CupertinoIcons.map_pin_ellipse,
-                          size: 50,
-                          color: Colors.blue[900],
-                        )
-                    ),
-                    onPressed: (){},
-                    label:
-                    Text(
-                      'There',
-                      style: TextStyle(
-                        fontFamily: 'Urbana',
-                        color: Colors.blue[900],
-                        fontSize: 20,
-                      ),
-                    ),
-                  style:
-                  ElevatedButton.styleFrom(
-                    minimumSize: const Size(375.0, 120.0),
-                    primary: Colors.white60,
-                  ),
-                ),
-                SizedBox(height: 15.0),
-                ElevatedButton.icon(
-                  icon: Container(
-                      alignment: Alignment.center,
-                      child: Icon(
-                        CupertinoIcons.book_fill,
-                        size: 50,
-                        color: Colors.blue[900],
-                      )
-                  ),
-                  onPressed: (){},
-                  label:
-                  Text(
-                    'General',
-                    style: TextStyle(
-                      fontFamily: 'Urbana',
-                      color: Colors.blue[900],
-                      fontSize: 20,
-                    ),
-                  ),
-                  style:
-                  ElevatedButton.styleFrom(
-                    minimumSize: const Size(375.0, 120.0),
-                    primary: Colors.white60,
-                  ),
-                ),
-                SizedBox(height: 15.0),
-                ElevatedButton.icon(
-                  icon: Container(
-                      alignment: Alignment.center,
-                      child: Icon(
-                        CupertinoIcons.news_solid,
-                        size: 50,
-                        color: Colors.blue[900],
-                      )
-                  ),
-                  onPressed: (){},
-                  label:
-                  Text(
-                    'Kenobi',
-                    style: TextStyle(
-                      fontFamily: 'Urbana',
-                      color: Colors.blue[900],
-                      fontSize: 20,
-                    ),
-                  ),
-                  style:
-                  ElevatedButton.styleFrom(
-                    minimumSize: const Size(375.0, 120.0),
-                    primary: Colors.white60,
-                  ),
-                ),
-              ]
             ),
-            SizedBox(width: 10.0),
+            SizedBox(height: 15.0),
+            ElevatedButton.icon(
+              icon: Container(
+                alignment: Alignment.center,
+                child: Icon(
+                  CupertinoIcons.map_pin_ellipse,
+                  size: 50,
+                  color: Colors.blue[900],
+                  )
+                ),
+              onPressed: (){},
+              label:
+                Text(
+                  'There',
+                  style: TextStyle(
+                    fontFamily: 'Urbana',
+                    color: Colors.blue[900],
+                    fontSize: 20,
+                  ),
+                ),
+              style:
+                ElevatedButton.styleFrom(
+                  minimumSize: const Size(375.0, 100.0),
+                  primary: Colors.white,
+              ),
+            ),
+            SizedBox(height: 15.0),
+            ElevatedButton.icon(
+              icon: Container(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    CupertinoIcons.book_fill,
+                    size: 50,
+                    color: Colors.blue[900],
+                  )
+              ),
+              onPressed: (){},
+              label:
+              Text(
+                'General',
+                style: TextStyle(
+                  fontFamily: 'Urbana',
+                  color: Colors.blue[900],
+                  fontSize: 20,
+                ),
+              ),
+              style:
+              ElevatedButton.styleFrom(
+                minimumSize: const Size(375.0, 100.0),
+                primary: Colors.white,
+              ),
+            ),
+            SizedBox(height: 15.0),
+            ElevatedButton.icon(
+              icon: Container(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    CupertinoIcons.news_solid,
+                    size: 50,
+                    color: Colors.blue[900],
+                  )
+              ),
+              onPressed: (){},
+              label:
+              Text(
+                'Kenobi',
+                style: TextStyle(
+                  fontFamily: 'Urbana',
+                  color: Colors.blue[900],
+                  fontSize: 20,
+                ),
+              ),
+              style:
+              ElevatedButton.styleFrom(
+                minimumSize: const Size(375.0, 100.0),
+                primary: Colors.white,
+              ),
+            ),
+            SizedBox(height: 15.0),
+            ElevatedButton.icon(
+              icon: Container(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    CupertinoIcons.news_solid,
+                    size: 50,
+                    color: Colors.blue[900],
+                  )
+              ),
+              onPressed: (){},
+              label:
+              Text(
+                '1Kenobi',
+                style: TextStyle(
+                  fontFamily: 'Urbana',
+                  color: Colors.blue[900],
+                  fontSize: 20,
+                ),
+              ),
+              style:
+              ElevatedButton.styleFrom(
+                minimumSize: const Size(375.0, 100.0),
+                primary: Colors.white,
+              ),
+            ),
+            SizedBox(height: 15.0),
+            ElevatedButton.icon(
+              icon: Container(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    CupertinoIcons.news_solid,
+                    size: 50,
+                    color: Colors.blue[900],
+                  )
+              ),
+              onPressed: (){},
+              label:
+              Text(
+                '2Kenobi',
+                style: TextStyle(
+                  fontFamily: 'Urbana',
+                  color: Colors.blue[900],
+                  fontSize: 20,
+                ),
+              ),
+              style:
+              ElevatedButton.styleFrom(
+                minimumSize: const Size(375.0, 100.0),
+                primary: Colors.white,
+              ),
+            ),
+            SizedBox(height: 15.0),
+            ElevatedButton.icon(
+              icon: Container(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    CupertinoIcons.news_solid,
+                    size: 50,
+                    color: Colors.blue[900],
+                  )
+              ),
+              onPressed: (){},
+              label:
+              Text(
+                '3Kenobi',
+                style: TextStyle(
+                  fontFamily: 'Urbana',
+                  color: Colors.blue[900],
+                  fontSize: 20,
+                ),
+              ),
+              style:
+              ElevatedButton.styleFrom(
+                minimumSize: const Size(375.0, 100.0),
+                primary: Colors.white,
+              ),
+            ),
+            SizedBox(height: 15),
           ]
-        )
+        ),
+      ),
+      )
       /***********************************
        * Second page - two column example
        **********************************/
@@ -328,10 +383,6 @@ void main() => runApp(MaterialApp(
       /***************************************
        * End second page - two column example
        **************************************/
-    ),
-
-  // ----------------------------------------------------------------------------
-  // END YOUTUBE TUTORIAL HERE
-  // ----------------------------------------------------------------------------
-
-));
+    );
+  }
+}
