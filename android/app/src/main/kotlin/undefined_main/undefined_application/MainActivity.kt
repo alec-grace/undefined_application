3212af10c243fba4739d2285ddd1ec737f813ae0
Alec <agrace072@protonmail.com>
2022-01-13 17:33:15 -0500

    Adding the rest of the project files so complete recreation is easier

---
+++ android/app/src/main/kotlin/undefined_main/undefined_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package undefined_main.undefined_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ build/app/generated/source/buildConfig/debug/undefined_main/undefined_application/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package undefined_main.undefined_application;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "undefined_main.undefined_application";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
